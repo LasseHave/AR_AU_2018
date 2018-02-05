d8603634ed54c47fa728f962e51a627f5681bf87
Lasse Have Nielsen <lasse@havenielsen.dk>
2018-02-05 10:18:17 +0100

    init commit.

---
+++ Assignment0/Assets/MeteorCollision.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorCollision : MonoBehaviour {
+
+	private GameObject sun;
+	private GameObject earth;
+	public ParticleSystem particleObject;
+	private bool hasCollided;
+
+
+	// Use this for initialization
+	void Start () {
+		particleObject = GetComponentInChildren<ParticleSystem> ();
+		particleObject.Stop();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		sun = GameObject.Find ("Sun");
+		earth = GameObject.Find ("Earth");
+
+		var earthPos = earth.transform.position;
+		var meteorPos = transform.position;
+
+		double dist = Mathf.Sqrt (Mathf.Pow (earthPos.x - meteorPos.x, 2)
+			+ Mathf.Pow (earthPos.y - meteorPos.y, 2)
+			+ Mathf.Pow (earthPos.z - meteorPos.z, 2));
+		float radMeteor = transform.localScale.x * 0.5F;
+		float radEarth = transform.localScale.x * sun.transform.localScale.x * 0.5F;
+
+		if (dist < (radEarth * 0.5 + radMeteor * 0.5)) {
+			var collisionPoint = ((meteorPos - earthPos) * radEarth) + earthPos;
+			Debug.Log ("They collided!");
+
+			if (!hasCollided) {
+				particleObject.transform.position = collisionPoint;
+				particleObject.Play ();
+				hasCollided = true;
+			}
+				
+		} else {
+			hasCollided = false;
+		}
+
+	}
+}
---
+++ Assignment0/Assets/MoonRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonRotation : MonoBehaviour {
+
+	private GameObject earth;
+	private GameObject particleCollision;
+
+	// Use this for initialization
+	void Start () {
+		earth = GameObject.Find ("Earth");
+		particleCollision = GameObject.Find ("MeteorSystem");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0f, 1f, .5f));
+		transform.RotateAround (earth.transform.position, Vector3.up, 20 * Time.deltaTime);
+	}
+}
