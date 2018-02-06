f0d53b9761f323ee6584eb7675d4e6caebe81a9c
Alexander Kledal <alex.kledal@gmail.com>
2018-02-06 13:30:49 +0100

    Add scripts

---
+++ Assignment0/Assets/Scripts/Assignment1/SpaceShuttle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShuttle : MonoBehaviour {
+
+	public GameObject landingStrip;
+	public GameObject quad;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var dist = Vector3.Dot(landingStrip.transform.position, this.transform.position);
+
+
+	}
+}
