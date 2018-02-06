17acc4e8a02e948e4b24896e57430ab94f80645c
Alexander Kledal <alex.kledal@gmail.com>
2018-02-06 14:01:44 +0100

    Working on lerping color

--- Assignment0/Assets/Scripts/Assignment1/SpaceShuttle.cs
+++ Assignment0/Assets/Scripts/Assignment1/SpaceShuttle.cs
@@ -7,14 +7,14 @@ public class SpaceShuttle : MonoBehaviour {
 	public GameObject landingStrip;
 	public GameObject quad;
 
-	//private Renderer quadShader;
+	private Renderer quadRenderer;
 
 	private Color red = new Color(255, 0, 0);
 	private Color green = new Color(0, 255, 0);
 
 	// Use this for initialization
 	void Start () {
-		//quadShader = quad.GetComponent<Renderer> ();
+		quadRenderer = quad.GetComponent<Renderer> ();
 	}
 	
 	// Update is called once per frame
@@ -22,12 +22,10 @@ public class SpaceShuttle : MonoBehaviour {
 		var dot = Vector3.Dot(landingStrip.transform.position.normalized, this.transform.position.normalized);
 		var normalizedDot = (dot + 1) / 2;
 
-		Color color = Color.Lerp (red, green, dot);
+		Color color = Color.Lerp (green, red, dot);
 
-		Debug.Log ("Dot: " + dot);
+		quadRenderer.material.color = color;
 
-		quad.GetComponent<Renderer> ().material.SetColor ("_Color", color);
-		//Debug.Log ("Norm Dist: " + normalizedDist);
 		Debug.Log ("color: " + color);
 	}
 }
