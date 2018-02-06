491920bf790989386dcc82ea5e071d2db78b606b
Unknown <lasse@havenielsen.dk>
2018-02-06 18:03:59 +0100

    Added scene and objects along script for subtask D

---
+++ Assignment0/Assets/Scripts/Assignment1/D_SpaceShuttle_EarthDetection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D_SpaceShuttle_EarthDetection : MonoBehaviour {
+
+	private GameObject earth;
+	private GameObject nose;
+	// Use this for initialization
+	void Start () {
+		earth = GameObject.Find ("Earth");
+		nose = GameObject.Find ("SpaceShuttleNose");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
