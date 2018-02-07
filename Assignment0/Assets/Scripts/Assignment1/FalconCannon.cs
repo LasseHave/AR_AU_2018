041fdce470c2945f925f8876b883e5b8b3fbc551
JesperHjort <jesper.hjortshoej@outlook.com>
2018-02-07 21:49:26 +0100

    work task e
    Now hits + explosion of simple laser
    +Added Assignment 0 solution

--- Assignment0/Assets/Scripts/Assignment1/FalconCannon.cs
+++ Assignment0/Assets/Scripts/Assignment1/FalconCannon.cs
@@ -2,18 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FalconCannon : MonoBehaviour {
-
-    public float rayLenght = 10;
-    public bool shoot_flag = false;
-    private Ray ray;
-    private Material material;
-
-    // When added to an object, draws colored rays from the
-    // transform position.
-    public int lineCount = 1;
-    public float radius = 3.0f;
+public class FalconCannon : MonoBehaviour
+{
+    public float rayLenght = 5;
+    public bool shoot_flag = false, hasCollided = false;
+    public GameObject particleObject;
+    public ParticleSystem ParticleSystem;
+    public RaycastHit hit;
     static Material lineMaterial;
+
+    // Use this for initialization
+    void Start()
+    {
+        particleObject = GameObject.Find("TieFighterExplosion");
+        ParticleSystem = particleObject.GetComponent<ParticleSystem>();
+        ParticleSystem.Stop();
+    }
+
+    private void performExplosion(Vector3 hit_point)
+    {
+        if (!hasCollided)
+        {
+            particleObject.transform.position = hit_point;// new Vector3(0,0,0);
+            ParticleSystem.Play(); hasCollided = true;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up"))
+            shoot_flag = true;
+        else
+            shoot_flag = false;
+    }
+
+    // Will be called after all regular rendering is done
+    public void OnRenderObject()
+    {
+        if (shoot_flag)
+        {
+            CreateLineMaterial();
+            // Apply the line material
+            lineMaterial.SetPass(0);
+
+            GL.Begin(GL.LINES);
+            GL.Color(Color.red);
+            GL.Vertex(transform.position);
+            GL.Vertex(transform.position + transform.forward * rayLenght);
+            GL.End();
+
+            if (Physics.Raycast(transform.position, transform.forward, out hit, rayLenght))
+            {
+                Debug.Log("Found an object - distance: " + hit.distance + hit.point);
+                Debug.Log("Found an object - Point: " + hit.point);
+                Debug.DrawLine(transform.position, hit.point);
+                performExplosion(hit.point);
+            }
+            else
+                hasCollided = false;
+        }
+    }
+
     static void CreateLineMaterial()
     {
         if (!lineMaterial)
@@ -30,75 +79,6 @@ public class FalconCannon : MonoBehaviour {
             lineMaterial.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
             // Turn off depth writes
             lineMaterial.SetInt("_ZWrite", 0);
-        }
-    }
-
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-        if (Input.GetKey("up")) {
-            shoot_flag = true;
-        }
-        else {
-            shoot_flag = false;
-        }
-        //ShootLaser();
-       // Debug.DrawRay(transform.position, transform.forward * rayLenght);
-        Debug.Log(ray.direction);
-	}
-    /*  void onRenderObject()
-      {
-          CreateLineMaterial();
-          // Apply the line material
-          lineMaterial.SetPass(0);
-          GL.PushMatrix();
-          GL.MultMatrix(transform.localToWorldMatrix);
-          GL.Begin(GL.LINES);
-          GL.Color(Color.red);
-          GL.Vertex(transform.position);
-          GL.Vertex(transform.position + transform.forward * rayLenght);
-          GL.End();
-          GL.PopMatrix();
-          Debug.Log("I was here");
-      }*/
-    // Will be called after all regular rendering is done
-    public void OnRenderObject()
-    {
-        if ((shoot_flag) && (this.isActiveAndEnabled))
-        {
-            CreateLineMaterial();
-            // Apply the line material
-            lineMaterial.SetPass(0);
-
-            GL.PushMatrix();
-            // Set transformation matrix for drawing to
-            // match our transform
-            GL.MultMatrix(transform.localToWorldMatrix);
-
-            // Draw lines
-            GL.Begin(GL.LINES);
-            //   for (int i = 0; i < lineCount; ++i)
-            // {
-            /*   float a = 1 / (float)lineCount;
-               float angle = a * Mathf.PI * 2;
-               // Vertex colors change from red to green
-               GL.Color(new Color(a, 1 - a, 0, 0.8F));
-               // One vertex at transform position
-               GL.Vertex3(0, 0, 0);
-               // Another vertex at edge of circle
-               GL.Vertex3(Mathf.Cos(angle) * radius, Mathf.Sin(angle) * radius, 0);*/
-            GL.Color(Color.red);
-            GL.Vertex(transform.position);
-            GL.Vertex(transform.position + transform.forward * rayLenght);
-
-            //}
-            GL.End();
-            GL.PopMatrix();
         }
     }
 }
