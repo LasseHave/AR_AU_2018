653020999eccb8473386772964b9598c3c0059aa
JesperHjort <jesper.hjortshoej@outlook.com>
2018-02-11 19:09:15 +0100

    Task D
    Updated World Center mode and implemented new solution for task D.
    appears to work if using the Falcon instead of shuttle.

--- Assignment0/Assets/Scripts/Assignment1/D_SpaceShuttle_EarthDetection.cs
+++ Assignment0/Assets/Scripts/Assignment1/D_SpaceShuttle_EarthDetection.cs
@@ -3,28 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class D_SpaceShuttle_EarthDetection : MonoBehaviour {
-	private GameObject earth;
-	private GameObject nose;
+	private GameObject earth, nose;
 	private string testText;
 	// Use this for initialization
 	void Start () {
 		earth = GameObject.Find ("Earth");
 		nose = GameObject.Find ("SpaceShuttle");
-		testText = "Init";
+        testText = "Init";
 	}
 
 	// Update is called once per frame
 	void Update () {
+
+        /* BY LASSE #####
 		var nose4 = T(nose.transform.localPosition.x, nose.transform.localPosition.y, nose.transform.localPosition.z);
 		var earth4 = T(earth.transform.localPosition.x, earth.transform.localPosition.y, earth.transform.localPosition.z);
 
 		var worldCoords = nose4*earth4; // Multiply as of the slides 
-
-
-
+        
 		string hemisphere = CalculateHemisphere (worldCoords, nose4);
 		testText = hemisphere;
-	}
+        */ // #####
+
+        //// Jesper Devs
+        // Part 1)
+        // From shuttle to world
+        Matrix4x4 Shuttle_M = transform.localToWorldMatrix;
+        // From Earth to world
+        Matrix4x4 Earth_M = earth.transform.localToWorldMatrix;
+        // Combine the transformation from Shuttle --> World --> Earth
+        Matrix4x4 Shuttle_Earth_Relation = Earth_M.inverse * Shuttle_M;
+        // Calculate the shuttle nose position in the earth coordinate system
+        Vector3 ShuttleNosePos_Earth = Shuttle_Earth_Relation.MultiplyPoint3x4(nose.transform.localPosition);
+     
+        // Part 2)
+        // Calculate the length of the vector drawn by x,z direction
+        var shuttleNose_dist = Mathf.Sqrt(Mathf.Pow(ShuttleNosePos_Earth.x,2) + Mathf.Pow(ShuttleNosePos_Earth.z, 2));
+        // Calculate the earth radius
+        var r_earth = earth.transform.localScale.x / 2;
+        // If the distance is within radius of the earth and shuttle is within 10 cm range we are howevering across earth
+        // Due to scale of the Earth we need to include this in the calculations
+        if (shuttleNose_dist * earth.transform.localScale.x < r_earth  && ShuttleNosePos_Earth.y * earth.transform.localScale.x < 0.10 )
+        {
+            // If Z value is positive we are in the northen hemisphere
+            if (ShuttleNosePos_Earth.z > 0)
+            {
+                testText = "North";
+            }
+            else // Otherwise south
+            {
+                testText = "South";
+            } 
+        } // Clear text if we are not howevering earth
+        else
+        {
+            testText = "";
+        }
+
+    }
 		
 
 	private void OnGUI()
