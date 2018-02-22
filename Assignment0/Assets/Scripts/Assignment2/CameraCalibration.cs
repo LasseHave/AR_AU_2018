31f72f03c424cdb43ad64df22c5361c2c248c851
JesperHjort <jesper.hjortshoej@outlook.com>
2018-02-22 09:09:00 +0100

    Updates to camera calibration
    Added near/far field calculations + updated code strcuture

--- Assignment0/Assets/Scripts/Assignment2/CameraCalibration.cs
+++ Assignment0/Assets/Scripts/Assignment2/CameraCalibration.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CameraCalibration : MonoBehaviour {
 
     public Matrix4x4 originalProjection;
+    float nearField = 0.05F, farField = 2F, width = 640F, height = 480F, 
+        fx = 833.88811F, fy = 834.98600F, cx = 319.52686F, cy = 238.81514F;
     Camera cam;
     // Use this for initialization
     void Start () {
@@ -13,14 +15,18 @@ public class CameraCalibration : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        originalProjection = cam.projectionMatrix;
-        originalProjection.m00 = 2 * 833.88811F / 640F;
-        originalProjection.m11 = 2 * 834.98600F / 480F;
-        originalProjection.m02 = (-2 * 319.52686F / 640F) + 1;
-        originalProjection.m12 = (-2 * 238.81514F / 480F) + 1;
+        originalProjection = cam.projectionMatrix; 
+        
+        // Set the projection matrix properties
+        originalProjection.m00 = 2 * fx / width;
+        originalProjection.m11 = 2 * fy / height;
+        originalProjection.m02 = (-2 * cx / width) + 1;
+        originalProjection.m12 = (-2 * cy / height) + 1;
         originalProjection.m32 = -1;
-
-
+        originalProjection.m22 = -((farField + nearField) / (farField - nearField));
+        originalProjection.m23 = -((2 * farField * nearField) / (farField - nearField));
+        Debug.Log("First coordinate: " + originalProjection.m22 + " Second coordinate: " + originalProjection.m23);
+        // Update camera projection matrix
         cam.projectionMatrix = originalProjection;
     }
 }
