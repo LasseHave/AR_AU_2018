b5dd1051cf9c238a7059a70ed68f03fe875c6ed9
Alexander Kledal <alex.kledal@gmail.com>
2018-03-12 23:44:38 +0100

    Working on Assignment 3 e.

---
+++ Assignment0/Assets/DrawButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class DrawButton : MonoBehaviour, IVirtualButtonEventHandler {
+
+	GameObject cam;
+
+	// Use this for initialization
+	void Start () {
+
+		cam = GameObject.Find ("ARCamera");
+
+		GameObject virtualButtonObject = GameObject.Find ("drawButton");
+		virtualButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+		
+	}
+
+	/// <summary>
+	/// Called when the virtual button has just been pressed:
+	/// </summary>
+	public void OnButtonPressed(VirtualButtonBehaviour vb) {
+		Debug.Log("button pressed");
+
+		cam.GetComponent<Assignment3_e> ().isDrawing = true;
+	}
+
+	/// <summary>
+	/// Called when the virtual button has just been released:
+	/// </summary>
+	public void OnButtonReleased(VirtualButtonBehaviour vb) { 
+		Debug.Log("button released");
+		cam.GetComponent<Assignment3_e> ().isDrawing = false;
+	}
+}
--- Assignment0/Assets/Scripts/Assignment3/Assignment3_e.cs
+++ Assignment0/Assets/Scripts/Assignment3/Assignment3_e.cs
@@ -9,24 +9,35 @@ public class Assignment3_e : MonoBehaviour {
 	Mat cameraImageMat;
 
 	[Range(0f, 180f)]
-	public int minH = 108;
+	public int minH = 95;
 	[Range(0f, 180f)]
-	public int maxH = 133;
+	public int maxH = 150;
 	[Range(0f, 255f)]
-	public int minS = 53;
+	public int minS = 43;
 	[Range(0f, 255f)]
-	public int maxS = 113;
+	public int maxS = 108;
 	[Range(0f, 255f)]
-	public int minV = 178;
+	public int minV = 119;
 	[Range(0f, 255f)]
-	public int maxV = 222;
+	public int maxV = 168;
 
 	public int blurSize = 5;
-	public int elementSize = 7;
+	public int elementSize = 5;
+
+
+	public bool isDrawing = false;
+
+	private GameObject drawPlate;
+	private Renderer rend;
+	private Texture2D newTexture;
 
 	// Use this for initialization
 	void Start () {
-		
+
+		drawPlate = GameObject.Find ("DrawPlate");
+
+		rend = drawPlate.GetComponent<Renderer>();
+		newTexture = Instantiate(rend.material.mainTexture) as Texture2D;
 	}
 	
 	// Update is called once per frame
@@ -52,6 +63,8 @@ public class Assignment3_e : MonoBehaviour {
 			Imgproc.medianBlur(hsv, hsv, blurSize);
 			Mat element = Imgproc.getStructuringElement (Imgproc.MORPH_ELLIPSE, new Size (2 * elementSize + 1, 2 * elementSize + 1), new Point (elementSize, elementSize));
 			Imgproc.dilate (hsv, hsv, element);
+
+
 			/// Contours
 
 			List<MatOfPoint> contours = new List<MatOfPoint> ();
@@ -70,9 +83,87 @@ public class Assignment3_e : MonoBehaviour {
 					largestIdx = i;
 				}
 			}
+
 			Imgproc.drawContours (cameraImageMat, contours, largestIdx, new Scalar (0, 0, 255), 1);
 
+			if (largestIdx != -1) {
+				var largeContour = contours [largestIdx].toList();
 
+				double minimumY = 10000;
+				Point finger = null;
+
+
+				for (var i = 0; i < largeContour.Count; i++) {
+					var po = largeContour [i];
+					if (po.y < minimumY) {
+						minimumY = po.y;
+						finger = po;
+					}
+				}
+
+
+				rend.material.mainTexture = newTexture;
+
+	
+				Vector3 p = new Vector3();
+				Camera  c = Camera.main;
+				Event   e = Event.current;
+				Vector2 mousePos = new Vector2();
+
+				// Get the mouse position from Event.
+				// Note that the y position from Event is inverted.
+				mousePos.x = (float)finger.x;
+				mousePos.y = (float)(c.pixelHeight - finger.y);
+
+				p = c.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 22) );
+
+				GameObject.Find ("Finger").transform.position = p;
+
+				Ray ray = c.ScreenPointToRay(new Vector3(mousePos.x, mousePos.y, 0));
+				Debug.DrawRay(ray.origin, ray.direction * 22, Color.yellow);
+
+				RaycastHit hit;
+				if (Physics.Raycast (ray, out hit)) {
+					Renderer rend = hit.transform.GetComponentInParent<Renderer>();
+
+					if (rend == null || rend.material == null || rend.material.mainTexture == null)
+						return;
+
+					Debug.Log ("HIT");
+
+					Texture2D tex = rend.material.mainTexture as Texture2D;
+					Vector2 pixelUV = hit.textureCoord;
+					pixelUV.x *= tex.width;
+					pixelUV.y *= tex.height;
+
+					Debug.Log (pixelUV);
+
+					tex.SetPixel((int)pixelUV.x, (int)pixelUV.y, Color.black);
+
+					rend.material.mainTexture = tex;
+				}
+
+				/*
+				Vector3 localPos = drawPlate.transform.InverseTransformPoint (p);
+				Debug.Log (localPos);
+
+				var height = 22;//rend.material.mainTexture.height;
+				var width = 22;//rend.material.mainTexture.width;
+
+				var texX = localPos.x / width * width;
+				var texY = localPos.y / height * height;
+
+				Debug.Log (texX + ":" + texY);
+
+				newTexture.SetPixel ((int)texX, (int)texY, Color.black);
+
+				// actually apply all SetPixels, don't recalculate mip levels
+				newTexture.Apply(false);
+				*/
+
+			}
+
+			/*
 			// Polygon / hull
 			if (contours.Count > 0) {
 				var contourPoints = contours [largestIdx].toList ();
@@ -91,15 +182,15 @@ public class Assignment3_e : MonoBehaviour {
 					points.Add (contourPoints [index]);
 
 				}
+
 				hull.Add ( new MatOfPoint(points.ToArray()) );
 
 				Imgproc.drawContours(cameraImageMat, hull, 0, new Scalar (0, 255, 0), 3);
 
-				if (points.ToArray ().Length > 2) {
-					
-				}
+				//if (points.ToArray ().Length > 2) {
+				//}
 			}
-
+			*/
 
 			MatDisplay.DisplayMat (cameraImageMat, MatDisplaySettings.FULL_BACKGROUND);
 			MatDisplay.DisplayMat (hsv, MatDisplaySettings.BOTTOM_LEFT);
